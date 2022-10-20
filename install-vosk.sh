994e6d3444a5090080e60821db3e57e209cabb5a
Maurício Collaça <mauriciocramos@gmail.com>
2022-10-20 01:45:02 -0300

    install vox and concerning models

--- install-vosk.sh
+++ install-vosk.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo '**********************************'
-echo 'install vosk (requires Python pip)'
-echo '**********************************'
-if [ -z "$1" ]
+echo '************'
+echo 'install vosk'
+echo '************'
   then
     echo 'ERROR: Missing conda environment name'
     exit 1
@@ -17,3 +16,16 @@ conda activate $1
 
 # vosk
 pip install vosk
+
+# download and unzip models
+wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -P /data/asr/models/vosk/
+wget https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip -P /data/asr/models/vosk/
+wget https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip -P /data/asr/models/vosk/
+wget https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip -P /data/asr/models/vosk/
+wget https://alphacephei.com/vosk/models/vosk-model-pt-fb-v0.1.1-20220516_2113.zip -P /data/asr/models/vosk/
+unzip /data/asr/models/vosk/vosk-model-small-en-us-0.15.zip -d /data/asr/models/vosk/
+unzip /data/asr/models/vosk/vosk-model-en-us-0.22.zip -d /data/asr/models/vosk/
+unzip /data/asr/models/vosk/vosk-model-en-us-0.22-lgraph.zip -d /data/asr/models/vosk/
+unzip /data/asr/models/vosk/vosk-model-small-pt-0.3.zip -d /data/asr/models/vosk/
+unzip /data/asr/models/vosk/vosk-model-pt-fb-v0.1.1-20220516_2113.zip -d /data/asr/models/vosk/
+rm /data/asr/models/vosk/*.zip
