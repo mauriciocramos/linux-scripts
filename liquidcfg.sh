0fbdc3298d2eafb69d76f686c60676d93a8146aa
Maurício Collaça <mauriciocramos@gmail.com>
2022-10-20 02:25:50 -0300

    customizable color

--- liquidcfg.sh
+++ liquidcfg.sh
@@ -6,8 +6,8 @@ if [ -z "$1" ]
      exit 1
 fi
 liquidctl --match gigabyte initialize --verbose
-liquidctl --match gigabyte set sync color fixed 0000ff --verbose
+liquidctl --match gigabyte set sync color fixed $3 --verbose
 liquidctl --match corsair initialize --pump-mode $1 --verbose
-liquidctl --match corsair set led color fixed 0000ff --verbose
+liquidctl --match corsair set led color fixed $3 --verbose
 liquidctl --match corsair set fan speed $2
 
