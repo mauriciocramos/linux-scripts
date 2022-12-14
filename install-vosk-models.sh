f3bb84edf53dc354ef7ad2f3312a4586121b2e40
Maurício Collaça <mauriciocramos@gmail.com>
2022-12-14 19:42:36 -0300

    download if not available

--- install-vosk-models.sh
+++ install-vosk-models.sh
@@ -4,22 +4,22 @@ echo '*******************'
 echo 'download vosk models'
 echo '*******************'
 echo
-echo 'WARNING: Previously downloaded models will be deleted.'
+echo 'WARNING: It donwloads vosk models but not needed after environment reinstallation.'
 echo
 read -rsp $'Press enter to continue...\n'
 
 VOSK_DIR=/data/asr/models/vosk/
-rm -rf "$VOSK_DIR"
-# download and unzip models
-wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -P $VOSK_DIR
-wget https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip -P $VOSK_DIR
-wget https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip -P $VOSK_DIR
-wget https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip -P $VOSK_DIR
-wget https://alphacephei.com/vosk/models/vosk-model-pt-fb-v0.1.1-20220516_2113.zip -P $VOSK_DIR
-# unzip "$VOSK_DIR"vosk-model-small-en-us-0.15.zip -d "$VOSK_DIR"
-# unzip "$VOSK_DIR"vosk-model-en-us-0.22.zip -d "$VOSK_DIR"
-# unzip "$VOSK_DIR"vosk-model-en-us-0.22-lgraph.zip -d "$VOSK_DIR"
-# unzip "$VOSK_DIR"vosk-model-small-pt-0.3.zip -d "$VOSK_DIR"
-# unzip "$VOSK_DIR"vosk-model-pt-fb-v0.1.1-20220516_2113.zip -d "$VOSK_DIR"
-unzip "$VOSK_DIR"*.zip -d "$VOSK_DIR"
-rm "$VOSK_DIR"*.zip
+# download models
+wget -N https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip -P $VOSK_DIR
+wget -N https://alphacephei.com/vosk/models/vosk-model-en-us-0.22.zip -P $VOSK_DIR
+wget -N https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip -P $VOSK_DIR
+wget -N https://alphacephei.com/vosk/models/vosk-model-en-us-0.42-gigaspeech.zip -P $VOSK_DIR
+wget -N https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip -P $VOSK_DIR
+wget -N https://alphacephei.com/vosk/models/vosk-model-pt-fb-v0.1.1-20220516_2113.zip -P $VOSK_DIR
+# unzip models
+unzip -u "$VOSK_DIR"vosk-model-small-en-us-0.15.zip -d "$VOSK_DIR"
+unzip -u "$VOSK_DIR"vosk-model-en-us-0.22.zip -d "$VOSK_DIR"
+unzip -u "$VOSK_DIR"vosk-model-en-us-0.22-lgraph.zip -d "$VOSK_DIR"
+unzip -u "$VOSK_DIR"vosk-model-en-us-0.42-gigaspeech.zip -d "$VOSK_DIR"
+unzip -u "$VOSK_DIR"vosk-model-small-pt-0.3.zip -d "$VOSK_DIR"
+unzip -u "$VOSK_DIR"vosk-model-pt-fb-v0.1.1-20220516_2113.zip -d "$VOSK_DIR"
