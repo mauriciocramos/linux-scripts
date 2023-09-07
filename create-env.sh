985294145eb0938d4c50a7f002c5d3bc0705e0dc
Maurício Collaça <mauriciocramos@gmail.com>
2023-09-07 15:50:25 -0300

    minor fixes

--- create-env.sh
+++ create-env.sh
@@ -3,7 +3,7 @@ clear
 echo '****************************************************'
 echo 'Conda Data Science environment creation'
 echo 'Author: Mauricio Ramos (mauriciocramos at gmail.com)'
-echo 'Date created: 17/8/2020 Date changed: 2/9/2022'
+echo 'Date created: 17/8/2020 Date changed: 7/9/2023'
 echo 'Usage: create-env.sh dev'
 echo '****************************************************'
 
@@ -13,27 +13,27 @@ if [ -z "$1" ]
     exit 1
 fi
 echo
-echo 'WARNING: Previously created environment '$1' will be removed'
+echo "WARNING: Previously created environment $1 will be removed"
 echo
 read -rsp $'Press enter to continue...\n'
 START_TIME=$(date +%s)
 
 # required to use conda within a script
-source $HOME/miniconda3/etc/profile.d/conda.sh
+source "$HOME"/miniconda3/etc/profile.d/conda.sh
 # remove previous environment
 conda deactivate
 conda info
-conda env remove --name $1
+conda env remove --name "$1"
 ENVDIR=$HOME/miniconda3/envs/
-rm $ENVDIR$1 -rf
-ls $ENVDIR
+rm "$ENVDIR$1" -rf
+ls "$ENVDIR"
 
 conda update -y -n base conda
 
 # Past packages still not installed: plotly qgrid pyaudio html5lib geopy google-api-python-client google-cloud-speech numpy-financial pyperclip
 
 # Base environment
-conda create --no-default-packages --override-channels -c conda-forge $2 -n $1 "python<3.11" numpy scipy pandas \
+conda create --no-default-packages --override-channels -c conda-forge "$2" -n "$1" "python<3.11" numpy scipy pandas \
 openpyxl \
 dnspython pymongo \
 ffmpeg \
@@ -47,28 +47,28 @@ selenium scrapy \
 python-confluent-kafka \
 tweepy
 
-conda activate $1
+conda activate "$1"
 
 # CUDA Toolkit (used by tensorflow, rapids, py-xgboost-gpu, spacy and pytorch)
-conda install --override-channels -c conda-forge -c nvidia $2 -n $1 cudatoolkit
+conda install --override-channels -c conda-forge -c nvidia "$2" -n "$1" cudatoolkit
 
 # RAPIDS https://rapids.ai/#quick-start
 #conda install --override-channels -c rapidsai -c conda-forge -c nvidia $2 -n $1 "rapids=23.06"
 
 # Tensorflow conda package is not built with tensorrt
-conda install --override-channels -c conda-forge $2 -n $1 tensorflow
+conda install --override-channels -c conda-forge "$2" -n "$1" tensorflow
 
 # Pytorch (cudatoolkit=11.8, ffmpeg.  Required by spacy on GPU) https://pytorch.org/get-started/locally/
-conda install --override-channels -c pytorch -c nvidia -c conda-forge $2 -n $1 pytorch torchvision torchaudio "pytorch-cuda=11.8"
+conda install --override-channels -c pytorch -c nvidia -c conda-forge "$2" -n "$1" pytorch torchvision torchaudio "pytorch-cuda=11.8"
 
 # NLP and ASR packages
-conda install --override-channels -c conda-forge $2 -n $1 nltk spacy spacy-transformers wordcloud gensim textblob langdetect speechrecognition pydub textstat
+conda install --override-channels -c conda-forge "$2" -n "$1" nltk spacy spacy-transformers wordcloud gensim textblob langdetect speechrecognition pydub textstat
 # Pip section
 pip install --upgrade pip
 pip install vosk
 pip install textatistic
 # https://www.adriangb.com/scikeras/stable/install.html#users-installation
-pip install --no-deps scikeras[tensorflow]
+pip install --no-deps "scikeras[tensorflow]"
 # TODO: replace scikeras by https://keras.io/keras_tuner/
 # pip install keras-tuner
 pip install kafka-python
@@ -78,5 +78,5 @@ conda config --set auto_activate_base false
 conda info
 
 END_TIME=$(date +%s)
-echo "Elapsed time: $(($END_TIME - $START_TIME)) seconds"
+echo "Elapsed time: $((END_TIME - START_TIME)) seconds"
 
