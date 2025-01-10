a3553d19d27646955cfde22747050877f1ca6f18
maurício <mauriciocramos@gmail.com>
2025-01-10 13:19:57 -0300

    disable debug

--- liquidcfg.sh
+++ liquidcfg.sh
@@ -5,9 +5,9 @@ if [ -z "$1" ]
     echo 'Example: liquidcfg quiet 100 0000ff'
      exit 1
 fi
-liquidctl --match gigabyte initialize --verbose
-liquidctl --match gigabyte set sync color fixed $3 --verbose
-liquidctl --match corsair initialize --pump-mode $1 --verbose
-liquidctl --match corsair set led color fixed $3 --verbose
+liquidctl --match gigabyte initialize
+liquidctl --match gigabyte set sync color fixed $3
+liquidctl --match corsair initialize --pump-mode $1
+liquidctl --match corsair set led color fixed $3
 liquidctl --match corsair set fan speed $2
 
