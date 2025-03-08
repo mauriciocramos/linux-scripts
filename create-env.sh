a3d529f8e3b4340ae586176ed004a6f6cf51e0b4
maurício <mauriciocramos@gmail.com>
2025-03-08 00:15:24 -0300

    disabling tensorflow

--- create-env.sh
+++ create-env.sh
@@ -30,59 +30,53 @@ ls "$ENVDIR"
 
 conda update -y -n base conda
 
-# Past packages: grid pyaudio html5lib geopy google-api-python-client google-cloud-speech numpy-financial tweepy pyperclip
-
-# Base environment RELEASE CONTROL
-# Numba: https://numba.readthedocs.io/en/stable/user/installing.html#version-support-information
-# AWS 2020-11-30: numba=0.52.x->python>=3.6<3.9->numpy>=1.15<1.20->llvmlite=0.35->LLVM=10->TBB>=2019.5<2020.3
-# rig 2023-06-21: numba=0.57.1->python>=3.8<3.12->numpy>=1.21<1.25-llvmlite=0.40->LLVM=14->TBB>=2021.6
-# rig 2024: python<3.10 jupyterlab<4 jupyterlab_execute_time<3
-conda create -n "$1" -c conda-forge --override-channels $2 --no-default-packages "python<3.13" \
+# Base environment
+conda create -n "$1" -c conda-forge --override-channels $2 --no-default-packages python \
 jupyterlab nodejs jupyterlab_execute_time jupyterlab-git jupyterlab_code_formatter autopep8 isort black \
-"numpy=1.26.4" scipy statsmodels \
+numpy scipy statsmodels \
 pandas openpyxl \
 matplotlib seaborn \
 scikit-learn \
-selenium scrapy \
 sqlalchemy \
 pymongo dnspython \
-python-confluent-kafka \
-pyspark \
+selenium scrapy \
 ffmpeg pydub \
+python-confluent-kafka \
 networkx nxviz pydot graphviz \
-great-expectations shap
+great-expectations \
+pyspark
 # boto3 \
 # trino-python-client \
 # sagemaker \
 # bokeh plotly \
 # numba dask \
 
-conda activate "$1" # mainly for late pip installations because conda's explicit --name "$1"
+conda activate "$1" # mainly for late pip installations because conda explicit --name "$1"
 
 # CUDA Toolkit (used by tensorflow, rapids, py-xgboost-gpu, spacy and pytorch)
-conda install -n "$1" -c conda-forge -c nvidia --override-channels $2  cudatoolkit
+conda install -n "$1" -c conda-forge -c nvidia --override-channels $2 cudatoolkit
 
-# RAPIDS https://rapids.ai/#quick-start
-# conda install -n "$1" -c rapidsai -c conda-forge -c nvidia --override-channels $2 "rapids=23.06"
+# Pytorch Conda packages are no longer available in pytorch conda channel: https://pytorch.org/get-started/locally/
+# Officially: pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# Alternative:
+conda install -n "$1" -c conda-forge --override-channels $2 pytorch-gpu torchvision torchaudio #"pytorch-cuda=11.8" no longer needed?
 
 # Tensorflow conda package is not built with tensorrt
-conda install -n "$1" -c conda-forge --override-channels $2 tensorflow
-
-# Pytorch (cudatoolkit=11.8, ffmpeg.  Required by spacy on GPU) https://pytorch.org/get-started/locally/
-conda install -n "$1" -c pytorch -c nvidia -c conda-forge --override-channels $2 pytorch torchvision torchaudio "pytorch-cuda=11.8"
+# Officially: pip install tensorflow
+# Alternative:
+# conda install -n "$1" -c conda-forge --override-channels $2 tensorflow
 
 # NLP packages
-conda install -n "$1" -c conda-forge --override-channels $2 nltk spacy spacy-transformers wordcloud gensim textblob langdetect textstat
+conda install -n "$1" -c conda-forge --override-channels $2 nltk spacy cupy spacy-transformers langchain shap wordcloud gensim textblob langdetect textstat
 
 echo '*******************************************'
 echo 'Pip installations after conda installations'
 echo '*******************************************'
 pip install --upgrade pip
-# pip install tensorflow
 pip install vosk
 # pip install textatistic # asked for gcc?
 # https://www.adriangb.com/scikeras/stable/install.html#users-installation
-pip install --no-deps "scikeras[tensorflow]" # TODO: replace by pip install keras-tuner https://keras.io/keras_tuner/
+# pip install --no-deps "scikeras[tensorflow]" # TODO: replace by pip install keras-tuner https://keras.io/keras_tuner/
 
 # post install
 conda config --set auto_activate_base false
