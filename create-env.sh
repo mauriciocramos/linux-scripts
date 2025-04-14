3a3fa6b5c85ae2ae03070d790a73e0e9bfdb7355
maurício <mauriciocramos@gmail.com>
2025-04-13 23:09:55 -0300

    Xet Storage suggested by HF's transformers

--- create-env.sh
+++ create-env.sh
@@ -81,6 +81,7 @@ echo '*******************************************'
 pip install --upgrade pip
 pip install vosk
 pip install leia
+pip install hf_xet # Xet Storage suggested by HF's Transformers
 # pip install textatistic # asked for gcc?
 # https://www.adriangb.com/scikeras/stable/install.html#users-installation
 # pip install --no-deps "scikeras[tensorflow]" # TODO: replace by pip install keras-tuner https://keras.io/keras_tuner/
