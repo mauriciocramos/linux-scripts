a1c344fba56d00698dac68b9bdb154af0c8c0704
mauricio <mauriciocramos@gmail.com>
2026-01-30 19:29:06 -0300

    comments

--- install-spacy-data-model.sh
+++ install-spacy-data-model.sh
@@ -11,26 +11,55 @@ if [ -z "$1" ]
 fi
 read -rsp $'Press enter to continue...\n'
 
-# export cond env downstream
-source "$HOME"/miniconda3/etc/profile.d/conda.sh
-
+source "$HOME"/miniconda3/etc/profile.d/conda.sh # required to use conda within a script
 conda deactivate
-# conda update -y -n base conda # uneeded
 conda activate $1
-
-# Spacy data and models from https://spacy.io/models/en
 
 pip install spacy-lookups-data
 
-# efficiency
+# ENGLISH MODELS
+
+# English pipeline optimized for CPU. Components: tok2vec, tagger, parser, senter, ner, attribute_ruler, lemmatizer.
+# vectors: 0 keys, 0 unique vectors (0 dimensions)
 python -m spacy download en_core_web_sm
-python -m spacy download xx_ent_wiki_sm
+
+# English pipeline optimized for CPU. Components: tok2vec, tagger, parser, senter, ner, attribute_ruler, lemmatizer.
+#  vectors: 685k keys, 20k unique vectors (300 dimensions)
+python -m spacy download en_core_web_md
+
+# English pipeline optimized for CPU. Components: tok2vec, tagger, parser, senter, ner, attribute_ruler, lemmatizer.
+# vectors: 685k keys, 343k unique vectors (300 dimensions)
+python -m spacy download en_core_web_lg
+
+# English transformer pipeline (Transformer(name=‘roberta-base’, piece_encoder=‘byte-bpe’, stride=104, type=‘roberta’,
+# width=768, window=144, vocab_size=50265)). Components: transformer, tagger, parser, ner, attribute_ruler, lemmatizer.
+# vectors: 0 keys, 0 unique vectors (0 dimensions)
+python -m spacy download en_core_web_trf
+
+# PORTUGUESE MODELS
+
+# Portuguese pipeline optimized for CPU. Components: tok2vec, morphologizer, parser, lemmatizer (trainable_lemmatizer),
+# senter, ner, attribute_ruler.
+# vectors: 0 keys, 0 unique vectors (0 dimensions)
 python -m spacy download pt_core_news_sm
 
-# accuracy
-python -m spacy download en_core_web_trf
-python -m spacy download xx_sent_ud_sm
+# Portuguese pipeline optimized for CPU. Components: tok2vec, morphologizer, parser, lemmatizer (trainable_lemmatizer),
+# senter, ner, attribute_ruler.
+# vectors: 500k keys, 20k unique vectors (300 dimensions)
+python -m spacy download pt_core_news_md
+
+# Portuguese pipeline optimized for CPU. Components: tok2vec, morphologizer, parser, lemmatizer (trainable_lemmatizer),
+# senter, ner, attribute_ruler.
+# vectors: 500k keys, 500k unique vectors (300 dimensions)
 python -m spacy download pt_core_news_lg
+
+# MULTI-LANGUAGE MODELS
+
+# Multi-language pipeline optimized for CPU. Components: ner.
+python -m spacy download xx_ent_wiki_sm
+
+# Multi-language pipeline optimized for CPU. Components: senter.
+python -m spacy download xx_sent_ud_sm
 
 # verify installation
 python -m spacy validate
